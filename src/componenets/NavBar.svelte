fb53fd6a5f9a78f184671f686dfd3ae4f60fb540
matt <matthew.j.goodwin.10june@gmail.com>
2025-01-27 20:11:15 +0000

    added global css file and navbar

---
+++ src/componenets/NavBar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "../styles/navBar.css"
+</script>
+
+<div class="topnav">
+    <button></button>
+    <p>Home Page</p>
+    <img src="" alt="pfp"/>
+</div>
---
+++ src/routes/CourseHome/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import{onMount} from "svelte";
+    import '../../styles/global.css'; // Import global styles
+    import '../../styles/homePage.css'; // Import styles
+    import NavBar from "../../componenets/NavBar.svelte";
+
+    onMount(() => {
+    //     let button: HTMLButtonElement = document.createElement('button');
+    //     button.innerHTML = 'Text';
+    //     button.classList.add('btn-red');
+    //     button.addEventListener('click', () => {
+    //         console.log('clicked');
+    //     })
+    //     document.querySelector('#main-div').appendChild(button);
+    });
+
+</script>
+
+<NavBar />
