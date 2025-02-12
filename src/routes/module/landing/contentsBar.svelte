26b4c389f27fca4578bc8886d7016e4ad60b9d30
matt <matthew.j.goodwin.10june@gmail.com>
2025-02-12 01:34:33 +0000

    contents bar created

--- src/routes/module/landing/contentsBar.svelte
+++ src/routes/module/landing/contentsBar.svelte
@@ -1,7 +1,70 @@
-<script land="ts">
+<script lang="ts">
     import '../../../styles/moduleLandingPage.css'; // Import styles
+    import Lesson from "./lessons.svelte"; //Import lessons
+
+    let lessons = [
+        {
+            "lesson_name": "fundamentals",
+            "lesson_id": 1,
+            "url": "example",
+            "sections":[
+                {
+                    "section_name": "intro"
+                },
+                {
+                    "section_name": "description"
+                },
+                {
+                    "section_name": "questions"
+                },
+                {
+                    "section_name": "docs"
+                }
+            ]
+        },
+        {
+            "lesson_name": "Osmosis",
+            "lesson_id": 2,
+            "url": "example",
+            "sections":[
+                {
+                    "section_name": "intro"
+                },
+                {
+                    "section_name": "description"
+                },
+                {
+                    "section_name": "questions"
+                },
+                {
+                    "section_name": "docs"
+                }
+            ]
+        },
+        {
+            "lesson_name": "reproduction",
+            "lesson_id": 3,
+            "url": "example",
+            "sections":[
+                {
+                    "section_name": "intro"
+                },
+                {
+                    "section_name": "description"
+                },
+                {
+                    "section_name": "questions"
+                },
+                {
+                    "section_name": "docs"
+                }
+            ]
+        },
+    ]
 </script>
 
 <div class="contents">
-
+    {#each lessons as lesson}
+        <Lesson info={lesson}/>
+    {/each}
 </div>
