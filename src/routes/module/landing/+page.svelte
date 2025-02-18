21bb106b145d977f02c5dd1af89eab2118a5a461
matt <matthew.j.goodwin.10june@gmail.com>
2025-02-18 15:45:15 +0000

    added functional module buttons linked to the module page

--- src/routes/module/landing/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script land="ts">
-    import '../../../styles/global.css'; // Import global styles
-    import '../../../styles/moduleLandingPage.css'; // Import styles
-    import Contents from './contentsBar.svelte' // Import contents bar
-</script>
-
-<Contents/>
---
+++ src/routes/modules/[id]/+page.svelte
@@ -0,0 +1,21 @@
+<script land="ts">
+    import '../../../styles/global.css'; // Import global styles
+    import '../../../styles/moduleLandingPage.css'; // Import styles
+    import Contents from './contentsBar.svelte' // Import contents bar
+
+    let { data } = $props();
+
+    // fetch("/v1/auth/login", {
+    //     method: "POST",
+    //     headers: {
+    //         "Authorization": "Bearer CUM"
+    //     },
+    //     body: JSON.stringify({
+    //         "email": "cum@balls.com",
+    //         "password": "penis"
+    //     })
+    // })
+</script>
+
+<h1>{data.module_id}</h1>
+<Contents/>
