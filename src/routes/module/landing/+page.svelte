e1d0357252edf827f8a69ee887688d53601946b6
Hugo Jenkins <27644618@students.lincoln.ac.uk>
2025-02-18 14:46:19 +0000

    added basic dashboard functionality and a new elelemnt

--- src/routes/module/landing/+page.svelte
+++ src/routes/module/landing/+page.svelte
@@ -2,6 +2,9 @@
     import '../../../styles/global.css'; // Import global styles
     import '../../../styles/moduleLandingPage.css'; // Import styles
     import Contents from './contentsBar.svelte' // Import contents bar
+    import LandingPageDashboard from './landingPageDashboard.svelte';
 </script>
+
+<LandingPageDashboard/>
 
 <Contents/>
---
+++ src/routes/module/landing/landingPageDashboard.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+    import '../../styles/moduleLandingPage.css'
+    let gradeCentreInfo = [{
+        "grade": [
+            {
+                "total": 80
+            }
+        ]
+    }]
+    export let totalGrade: number = gradeCentreInfo[0].grade[0].total
+    let announcemntsInfo = [{
+        "announcements": [
+            {
+                "title": "new assessment released",
+                "course": "Algorithms and Complexity"
+            },
+            {
+                "title": "new assessment released",
+                "course": "Algorithms and Complexity"
+            },
+            {
+                "title": "new assessment released",
+                "course": "Algorithms and Complexity"
+            }
+        ]
+    }]
+
+    let callenderInfo = [{
+        "callender": [
+            {
+                "title": "Algorithms ass 1",
+                "course": "Algorithms and Complexity",
+                "date": "2025-2-20"
+            },
+            {
+                "title": "Algorithms ass 1",
+                "course": "Algorithms and Complexity",
+                "date": "2025-2-20"
+            },
+            {
+                "title": "Algorithms ass 1",
+                "course": "Algorithms and Complexity",
+                "date": "2025-2-20"
+            },
+            {
+                "title": "Algorithms ass 1",
+                "course": "Algorithms and Complexity",
+                "date": "2025-2-20"
+            },
+            {
+                "title": "Algorithms ass 1",
+                "course": "Algorithms and Complexity",
+                "date": "2025-2-20"
+            }
+        ]
+    }]
+    let courseInfo = [{
+        "professors": [
+            {
+                "title": "Professor J. Doe"
+            },
+            {
+                "title": "Professor S. Butcher"
+            },
+            {
+                "title": "Professor J. Kennedy"
+            }
+        ],
+        "links": [
+            {
+                "linkTitle" : "important video",
+                "link": "https://www.youtube.com/watch?v=LQ4w9xiHkrY"
+            },
+            {
+                "linkTitle" : "important video",
+                "link": "https://www.youtube.com/watch?v=LQ4w9xiHkrY"
+            },
+            {
+                "linkTitle" : "important video",
+                "link": "https://www.youtube.com/watch?v=LQ4w9xiHkrY"
+            }
+        ]
+    }]
+</script>
+
+<div class = "dashboardHeader">
+    <h1>Dashboard</h1>
+    <p class="divider"></p>
+</div>
+
+<div id="dashboard" class="dashboard">
+    <div class="announcements">
+        {#each announcemntsInfo[0]["announcements"] as an}
+            <h4>{an.title}</h4>
+            <p>{an.course}</p>
+        {/each}
+    </div>
+
+    <div class = "grades">
+        <h1>{totalGrade}%</h1>
+    </div>
+
+    <div class = "info">
+        <h4>professors:</h4>
+        {#each courseInfo[0]["professors"] as ca}
+            <p>{ca.title}</p>
+        {/each}
+        <h4>useful links:</h4>
+        {#each courseInfo[0]["links"] as ca}
+            <a href = {ca.link}>{ca.linkTitle}</a>
+        {/each}
+    </div>
+
+    <div class = "calendar">
+        {#each callenderInfo[0]["callender"] as ca}
+            <h4>{ca.title}</h4>
+            <p>{ca.course} {ca.date}</p>
+        {/each}
+    </div>
+</div>
+
