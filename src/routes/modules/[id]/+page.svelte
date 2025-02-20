b423122543e1eae420277c1e78af9575a8ef8d76
matt <matthew.j.goodwin.10june@gmail.com>
2025-02-20 01:26:14 +0000

    fixed merge errors

--- src/routes/modules/[id]/+page.svelte
+++ src/routes/modules/[id]/+page.svelte
@@ -2,18 +2,11 @@
     import '../../../styles/global.css'; // Import global styles
     import '../../../styles/moduleLandingPage.css'; // Import styles
     import Contents from './contentsBar.svelte' // Import contents bar
-<<<<<<< HEAD:src/routes/module/landing/+page.svelte
     import LandingPageDashboard from './landingPageDashboard.svelte';
 </script>
 
+let { data } = $props();
+
 <LandingPageDashboard/>
-
 <Contents/>
-=======
-
-    let { data } = $props();
-
-</script>
-
 <Contents id="{data.module_id}"/>
->>>>>>> development:src/routes/modules/[id]/+page.svelte
