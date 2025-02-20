3c78fdcc804c19797f3e0301159e366d317fdd3a
matt <matthew.j.goodwin.10june@gmail.com>
2025-02-20 03:36:28 +0000

    contents bar changes from fullscreen element to sidebar element

--- src/routes/modules/[id]/+page.svelte
+++ src/routes/modules/[id]/+page.svelte
@@ -14,18 +14,27 @@
         for (const btn: HTMLElement of lessonButtons) {
             btn.addEventListener("click", (event: Event) => {
                 const target = event.target as HTMLButtonElement;
-                if (!loadedLessons.includes(target.dataset.lesson_id as number)) {
-                    loadedLessons = [...loadedLessons, target.dataset.lesson_id as number];
-                }
-                for(let id of loadedLessons) {
-                    let div: HTMLElement | null = document.getElementById(`blocks-${id}`);
-                    if (div) {
-                        document.getElementById("cached-lesson").appendChild(div);
+                if (!target.classList.contains("active")) {
+                    for (const btn: HTMLElement of lessonButtons) {
+                        btn.classList.remove("active")
+                    }
+                    target.classList.add("active")
+                    if (!loadedLessons.includes(target.dataset.lesson_id as number)) {
+                        loadedLessons = [...loadedLessons, target.dataset.lesson_id as number];
+                    }
+                    for (let id of loadedLessons) {
+                        let div: HTMLElement | null = document.getElementById(`blocks-${id}`);
+                        if (div) {
+                            document.getElementById("cached-lesson").appendChild(div);
+                        }
+                        let block_div: HTMLElement | null = document.getElementById(`blocks-${target.dataset.lesson_id}`);
+                        if (block_div) {
+                            document.getElementById("loaded-lesson").appendChild(block_div);
+                        }
                     }
                 }
-                let block_div: HTMLElement | null = document.getElementById(`blocks-${target.dataset.lesson_id}`);
-                if (block_div) {
-                    document.getElementById("loaded-lesson").appendChild(block_div);
+                else {
+                    document.querySelector(".contents").classList.toggle("small")
                 }
             })
         }
