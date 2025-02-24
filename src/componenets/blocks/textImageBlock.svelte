f38d41343b50c04e3ed64c7ff61cea18c9057fa0
FNASmileys <hugofraserj@gmail.com>
2025-02-24 16:56:03 +0000

    fixed image display

--- src/componenets/blocks/textImageBlock.svelte
+++ src/componenets/blocks/textImageBlock.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 
     import "../../styles/block-styles/text-image-block.css"
+    import image from "../../assets/Bliss_location,_Sonoma_Valley_in_2006.jpg"
 
     let data = [{
         "data": {
@@ -12,8 +13,8 @@
                 "But that's even worse.\n" +
                 "\n" +
                 "The post went live at 11:38 PM on July 16, and a mere 20 minutes later, the Burger King in question was alerted to the rogue employee. At least, I hope he's rogue. How did it happen? Well, the BK employee hadn't removed the Exif data from the uploaded photo, which suggested the culprit was somewhere in Mayfield Heights, Ohio. This was at 11:47. Three minutes later at 11:50, the Burger King branch address was posted with wishes of happy unemployment. 5 minutes later, the news station was contacted by another 4channer. And three minutes later, at 11:58, a link was posted: BK's \"Tell us about us\" online forum. The foot photo, otherwise known as exhibit A, was attached. Cleveland Scene Magazine contacted the BK in question the next day. When questioned, the breakfast shift manager said \"Oh, I know who that is. He's getting fired.\" Mystery solved, by 4chan. Now we can all go back to eating our fast food in peace.\n",
-            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fopen.spotify.com%2Falbum%2F0NgajUfdzTNxGEBR0m5iml&psig=AOvVaw1o3NKRm2k9DVV2iZS42doU&ust=1740067409769000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN-riO0IsDFQAAAAAdAAAAABAE",
-            "altText": "Big Poo The Cat"
+            "image": image,
+            "altText": "Bliss location, Sonoma Valley in 2006"
         }
     }]
 </script>
