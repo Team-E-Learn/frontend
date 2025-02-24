309df79cb1e7e573eab876876ebccff6e76e25bf
FNASmileys <hugofraserj@gmail.com>
2025-02-24 17:49:07 +0000

    added quiz block functionality

--- src/componenets/blocks/quizBlock.svelte
+++ src/componenets/blocks/quizBlock.svelte
@@ -4,8 +4,43 @@
 
     let data = [{
         "data": {
-            "": ""
+            "question": "press option A",
+            "options" : {
+                "A": {
+                    "text": "Option A",
+                    "isCorrect": true
+                },
+                "B": {
+                    "text": "Option B",
+                    "isCorrect": false
+                },
+                "C": {
+                    "text": "Option C",
+                    "isCorrect": false
+                },
+                "D": {
+                    "text": "Option D",
+                    "isCorrect": false
+                }
+            }
         }
     }]
 
+    function isAnswer(answer: boolean) {
+        if (answer == true) {
+            alert("correct")
+        } else {
+            alert("incorrect")
+        }
+    }
+
 </script>
+
+<div class="quiz-block">
+    <h3>{data[0]["data"]["question"]}</h3>
+    <div class="options">
+        {#each Object.values(data[0]["data"]["options"]) as option}
+            <button on:click={() => isAnswer(option.isCorrect)}>{option.text}</button>
+        {/each}
+    </div>
+</div>
