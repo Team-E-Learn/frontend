c823c812a338a6150a23e4956d3dc85455961030
FNASmileys <hugofraserj@gmail.com>
2025-02-26 12:13:35 +0000

    changed file names according to style guide

--- src/routes/tempBlockDisplay/+page.svelte
+++ src/routes/tempBlockDisplay/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import "../../styles/block-system.css";
-    import DownloadBlock from "../../componenets/blocks/downloadBlock.svelte";
-    import QuizBlock from "../../componenets/blocks/quizBlock.svelte";
-    import TextBlock from "../../componenets/blocks/textBlock.svelte";
-    import TextImageBlock from "../../componenets/blocks/textImageBlock.svelte";
-    import ImageBlock from "../../componenets/blocks/imageBlock.svelte";
+    import DownloadBlock from "../../componenets/blocks/download-block.svelte";
+    import QuizBlock from "../../componenets/blocks/quiz-block.svelte";
+    import TextBlock from "../../componenets/blocks/text-block.svelte";
+    import TextImageBlock from "../../componenets/blocks/text-image-block.svelte";
+    import ImageBlock from "../../componenets/blocks/image-block.svelte";
 </script>
 
-<TextBlock />
-<ImageBlock />
-<TextImageBlock />
-<DownloadBlock />
-<QuizBlock />
+<div class = "block-container">
+    <TextBlock />
+    <ImageBlock />
+    <TextImageBlock />
+    <DownloadBlock />
+    <QuizBlock />
+</div>
+
