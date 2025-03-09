a48191450d13ad21ba0e87786480aee3ba4d1841
FNASmileys <hugofraserj@gmail.com>
2025-03-09 17:25:55 +0000

    changed all blocks in edit-blocks to use interfaces

--- src/componenets/edit-blocks/text-block-edit.svelte
+++ src/componenets/edit-blocks/text-block-edit.svelte
@@ -2,7 +2,14 @@
 
     import "../../styles/block-styles/text-block.css"
 
-    let data = [{
+    interface textBlock {
+        data: {
+            title: string
+            text: string
+        }
+    }
+
+    let data: textBlock = [{
         "data": {
             "title": "Lorem Ipsum",
             "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
--- src/componenets/edit-blocks/text-image-block-edit.svelte
+++ src/componenets/edit-blocks/text-image-block-edit.svelte
@@ -3,18 +3,27 @@
     import "../../styles/block-styles/text-image-block.css"
     import image from "../../assets/Bliss_location,_Sonoma_Valley_in_2006.jpg"
 
-    let data = [{
+    interface textImageBlock {
+        data: {
+            title: string
+            text: string
+            image: string
+            altText: string
+        }
+    }
+
+    let data: textImageBlock = {
         "data": {
             "title": "Lorem Ipsum",
             "text": " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "image": image,
             "altText": "Bliss location, Sonoma Valley in 2006"
         }
-    }]
+    }
 </script>
 
 <div class = "text-image-block">
-    <h3>{data[0]["data"]["title"]}</h3>
-    <p>{data[0]["data"]["text"]}</p>
-    <img src={data[0]["data"]["image"]} alt={data[0]["data"]["altText"]}>
+    <h3>{data["data"]["title"]}</h3>
+    <p>{data["data"]["text"]}</p>
+    <img src={data["data"]["image"]} alt={data["data"]["altText"]}>
 </div>
